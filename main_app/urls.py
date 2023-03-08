7cf4e0f9c830ef8e41894cc04de2698e36792850
danieljo <hyundanieljo@gmail.com>
2023-03-08 10:02:52 -0500

    added path for postcreate, update delete, update views and models

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('posts/', views.posts_index, name='index'),
+    path('posts/create/', views.PostCreate.as_view(), name='posts_create'),
+    path('posts/<int:pk>/update/', views.PostUpdate.as_view(), name='posts_update'),
+    path('posts/<int:pk>/delete/', views.PostDelete.as_view(), name='posts_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 
 # Create your views here.
@@ -9,3 +10,18 @@ def home(request):
 def posts_index(request):
     posts = Post.objects.all()
     return render(request, 'posts/index.html', { 'posts': posts })
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'body']
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = '/posts/'
