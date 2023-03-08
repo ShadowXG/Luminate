c862674cd438b7e3eadf32bac16e670805824ec0
danieljo <hyundanieljo@gmail.com>
2023-03-08 17:05:25 -0500

    commit added class meta for ordering in models.py and updated post index.html

--- main_app/models.py
+++ main_app/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     likes = models.ManyToManyField(User, through='Like', related_name='post_likes')
+
+    class Meta:
+        ordering = ['-created_date'] 
     
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
